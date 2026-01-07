873d5d200504dcb17c4183714cba1a0819e5735b
Fox003 <focloutier@gmail.com>
2026-01-07 12:32:15 -0500

    bibo upgrade et correction modifier de cooldown

--- Assets/Scripts/Gameplay/Player/Systems/PlayerCooldownSystem.cs
+++ Assets/Scripts/Gameplay/Player/Systems/PlayerCooldownSystem.cs
@@ -13,11 +13,13 @@ partial struct PlayerCooldownSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (weaponState, playerStats) in SystemAPI.Query<RefRW<WeaponState>, RefRO<PlayerBaseStats>>())
+        foreach (var (weaponState, playerStats, playerModifiers) in SystemAPI.Query<RefRW<WeaponState>, RefRO<PlayerBaseStats>, RefRO<PlayerStatsModifiers>>())
         {
             var weaponStats = playerStats.ValueRO.WeaponStats;
+            var modStats = playerModifiers.ValueRO.WeaponStatsModifiers;
+            var modifiedCooldown = ModifierUtils.CalculateModifiedStat(weaponStats.CooldownTime, modStats.CooldownModifier);
 
-            if (weaponState.ValueRO.CooldownData.CurrentCooldownTime < weaponStats.CooldownTime)
+            if (weaponState.ValueRO.CooldownData.CurrentCooldownTime < modifiedCooldown)
             {
                 weaponState.ValueRW.CooldownData.CurrentCooldownTime += SystemAPI.Time.DeltaTime;
                 weaponState.ValueRW.CooldownData.isOnCooldown = true;
